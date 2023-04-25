3761331f82dbfb7339170721e00724bb4968dfa2
JeffersonFroes <jrmachadofroes@gmail.com>
2023-04-25 00:29:30 -0300

    Aula 09

---
+++ Aula10ex02.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Aula10ex02 {
+    public static void trocaVetor(int[]v){
+        int troca = 0;
+        for(int i = 0; i < v.length - 1; i++){
+            troca = v[i];
+            v[i] = v[i+1];
+            v[i+1] = troca;
+        }
+    }    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Informe o tamanho do vetor");
+        int n = in.nextInt();
+        int [] v1 = new int[n];
+
+        for(int i = 0; i < v1.length; i++){
+            System.out.println("Informe o valor " + i + " do Vetor");
+            v1[i] = in.nextInt();
+        }
+        trocaVetor(v1);
+        for(int i = 0; i < v1.length; i++){
+            System.out.print(v1[i]+ ", ");
+        }
+    }
+}
