930e2e1b69e09c51007a3771445139806a6ac00f
JeffersonFroes <62813467+froes1996@users.noreply.github.com>
2023-05-07 19:06:45 -0300

    ex

--- Metodos2/ex02.java
+++ Metodos2/ex02.java
@@ -13,11 +13,27 @@ package Metodos2;
 import java.util. Scanner;
 public class ex02 {
     public static void modificaVetor(int [] v){
-
+        int resultado = 1;
+        for(int i = 0; i < v.length; i++){
+            if(i % 2 == 0){
+                 v [i] = v[i] * 2;
+            }else{
+                v[i] = v[i] * 3;
+            }
+        }
+        printVetor(v);
+    }
+    public static void printVetor(int []v){
+        for(int i = 0; i < v.length;i++){
+        System.out.println("Posição " + i + " = " + v[i]);
+        }
     }
 
     public static void main(String[] args) {
         
+        int [] v = {1,2,3,4,5,6,7,8,9,10};
+
+        modificaVetor(v);
         
     }
 }
