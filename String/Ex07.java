07bd50bc4927f29ddcca6e937911560789f6b8bc
JeffersonFroes <62813467+froes1996@users.noreply.github.com>
2023-05-07 23:41:57 -0300

    ex

--- String/Ex07.java
+++ String/Ex07.java
@@ -1,3 +1,16 @@
+/*
+ Elabore um programa para ler do teclado um número de telefone como
+uma String no formato (555) 555-5555
+• O programa deve validar o telefone e caso não esteja no formato correto,
+um novo número deve ser solicitado
+• Depois utilize o método split para extrair o código de área como um token,
+os três primeiros dígitos do número de telefone como um segundo token e
+os últimos quatro dígitos do número de telefone como um terceiro token
+• O array retornado pelo método split deve ter 3 elementos
+• Remova os parênteses do primeiro token utilizando o método replaceAll
+• Imprima na tela o telefone concatenando todos os tokens: 5555555555
+ */
+
 import java.util.Scanner;
 public class Ex07 {
     public static void main(String[] args) {
