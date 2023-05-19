9dd20150cc1c33ec4d6cab884d0195061251f2c7
JeffersonFroes <62813467+froes1996@users.noreply.github.com>
2023-05-19 14:55:32 -0300

    ex

--- Revisão/Ex01.java
+++ Revisão/Ex01.java
@@ -1,41 +1,37 @@
 package Revisão;
+
 import java.util.Scanner;
+
 public class Ex01 {
-    public static int calculaFatorial(int n){
+    public static int calculaFatorial(int n) {
         int fatorial = 1;
-        for(int i = 1; i <= n; i++){
-            fatorial = fatorial*i;
+        for (int i = 1; i <= n; i++) {
+            fatorial = fatorial * i;
         }
         return fatorial;
     }
-    public static double converteEmRadianos(double graus){
+
+    public static double converteEmRadianos(double graus) {
         double radianos = 0;
         radianos = (graus * Math.PI) / 180;
         return radianos;
     }
 
-    public static double calculaSeno(double G){
+    public static double calculaSeno(double G) {
         double seno = 0;
         double r = converteEmRadianos(G);
-        for(int i = 0; i <=5; i++){
-            seno = seno + Math.pow(-1, i) / (calculaFatorial(2*i+1)) * Math.pow(r, 2*i+1);
+        for (int i = 0; i <= 5; i++) {
+            seno = seno + Math.pow(-1, i) / (calculaFatorial(2 * i + 1)) * Math.pow(r, 2 * i + 1);
         }
         return seno;
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        System.out.println("Informe um numero");
-        int valor = in.nextInt();
-        System.out.println(calculaFatorial(valor));
-
-        System.out.println("Informe um valor em graus");
-        double graus = in.nextDouble();
-        System.out.println(converteEmRadianos(graus));
 
         System.out.println("Informe um angulo");
         int G = in.nextInt();
         System.out.println(calculaSeno(G));
     }
-    
+
 }
